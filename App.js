dcdc6bbb8656d12b45d8acb9662c08b06b69237f
Melike <79509231+me-likee@users.noreply.github.com>
2022-10-21 11:16:45 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {SafeAreaView,View,Text,Switch, FlatList } from "react-native";
+const data = [ 
+  {id:0, name: "Cafe 1",isFavorite: true,},
+  {id:1,name: "John",isFavorite: false}, 
+  {id:2,name:"Jane",isFavorite:true},
+  {id:3,name:"Liam",isFavorite:false}, 
+  {id:4,name:"Noah",isFavorite:true},
+  {id:5,name: "Jack",isFavorite: false,}, 
+];
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App()
+{
+  const[CafeList,SetCafeList]=React.useState(data);//Liste İçin
+  const[ShowOnlyFavorites,SetShowOnlyFavorites]=React.useState(false);//Switch İçin
+  function OnFavoritesChange(isFavoriteSelected)
+  {
+    SetShowOnlyFavorites(isFavoriteSelected);// Switch hareketini sağlar
+    isFavoriteSelected
+    ? SetCafeList(CafeList.filter(cafe=>cafe.isFavorite))
+    : SetCafeList(data)
+  }
+  return(
+      <SafeAreaView>
+        <View>
+           <Text style={{fontSize:40,marginTop:10}}>Favorileri Göster</Text>
+           <Switch style={{width:100,height:25}} value={ShowOnlyFavorites} onValueChange={OnFavoritesChange}/>
+          <FlatList data={CafeList}
+            renderItem={({item}) =><Text style={{fontSize:20,margin:10}}>{item.name}</Text>}/>
+        </View>
+      </SafeAreaView>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
